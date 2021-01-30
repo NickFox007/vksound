abe728a7c25d6022398385c2233a161ffe115353
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2021-01-30 14:44:30 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+app_username = '792727000000'				 		# Логин страницы
+app_password = 'password'							# Пароль страницы
+app_secret_key = b'mybestsecretkeyintheworld'		# Секретный ключ (значение взять из головы)
+app_limit = 20										# Лимит количества аудиозаписей при прогрузке
+app_host = "127.0.0.1"								# IP
+app_port = 8890										# Порт
